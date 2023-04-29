fbf9df3aea24739f3e6ef61970e0e9b470163d9b
Josh Hoover <caboodlennm@gmail.com>
2023-04-28 23:31:04 -0500

    Remove dev script

--- hook.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-curl -H "Content-Type: application/json" -d '{}' http://localhost:5555/checkAuth
-# curl -H "Content-Type: application/json" -d '{ "id": "715772891643510794" }' http://inharmonious.floomby.us:5555/discord
